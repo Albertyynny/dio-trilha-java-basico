aa8ca2ea03a138ef5b7ce1e49975e5434e6cd982
Albertyynny <51037790+Albertyynny@users.noreply.github.com>
2025-04-04 22:11:43 -0300

    sistema ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        String agencia;
+        String numero;
+        String nomeCliente;
+        double saldo;
+        
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta: ");
+        numero = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        nomeCliente = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo: ");
+        saldo = scanner.nextDouble();
+        
+        scanner.close();
+        
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + 
+                          agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque";
+        
+        System.out.println(mensagem);
     }
 }
